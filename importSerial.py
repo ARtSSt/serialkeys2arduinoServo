321ef98c502a40487c1397ca240d252ff0caab5c
ARtSSt <jpaden@ucsd.edu>
2024-02-27 12:21:51 -0800

    Update importSerial.py

--- importSerial.py
+++ importSerial.py
@@ -13,11 +13,11 @@ try:
         if keyboard.is_pressed('right arrow'):
             print("Moving servo right")
             send_command('R')
-            time.sleep(0.1)  # Delay for stability
+            time.sleep(1)  # Delay for stability
         elif keyboard.is_pressed('left arrow'):
             print("Moving servo left")
             send_command('L')
-            time.sleep(0.1)  # Delay for stability
+            time.sleep(1)  # Delay for stability
 
 except KeyboardInterrupt:
     print("Program terminated")
